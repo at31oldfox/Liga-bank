4be70808528d8649b1a80bec65569f459c03cd2c
Таня <tanamalceva274@gmail.com>
2021-09-16 15:21:55 +0300

    Добавит попап

--- project/src/components/main-page/main-page.jsx
+++ project/src/components/main-page/main-page.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
+import {useState} from 'react';
 import Header from '../header/header';
 import PromoSlider from '../promo-slider/promo-slider';
 import PopupLogin from '../popup-login/popup-login';
 
 export default function MainPage() {
+  const [isPopupActive, setPopupStatus] = useState(false);
+
+  const bodyElement = document.querySelector('body');
+
+  const onEscKeyDown = (evt, func) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      setPopupStatus(false);
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
+  if (isPopupActive) {
+    bodyElement.classList.add('page__body--unactive');
+  }
+
+  if (!isPopupActive && bodyElement.classList.contains('page__body--unactive')) {
+    bodyElement.classList.remove('page__body--unactive')
+  }
+
   return (
     <>
-      <Header />
+      <Header onLoginClick={() => setPopupStatus(true)} onKeyDown={onEscKeyDown}/>
       <main className="page-main">
         <PromoSlider />
-        <PopupLogin />
+        {isPopupActive && <PopupLogin onCloseClick={() => setPopupStatus(false)} onKeyDown={onEscKeyDown}/>}
       </main>
     </>
   );
